70f407967f37a3536ac5df5bad507298ea0350e6
ELIZA206 <sergeeff.andrew2018@yandex.ru>
2023-07-01 23:17:27 +0300

    Create App.java

---
+++ task01/src/main/java/ru/itis/App.java
@@ -0,0 +1,31 @@
+package ru.itis;
+
+import ru.itis.repositories.EventsRepository;
+import ru.itis.repositories.EventsRepositoryFileImpl;
+import ru.itis.repositories.UsersRepository;
+import ru.itis.repositories.UsersRepositoryFileImpl;
+import ru.itis.services.AppService;
+
+import java.time.LocalDate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        UsersRepository usersRepository = new UsersRepositoryFileImpl("users.txt");
+        EventsRepository eventsRepository =new EventsRepositoryFileImpl("events.txt","events_users.txt");
+        AppService appService =new AppService(usersRepository,eventsRepository);
+  //      appService.signUp("admin@gmail.com", "qwerty007");
+  //      appService.signUp("marsel.gmail.com", "qwerty008");
+  //      appService.addEvent("Практика по Java", LocalDate.now());
+  //      appService.addEvent("Практика по Golang", LocalDate.now().plusDays(1));
+        appService.addUserToEvent("marsel@gmail.com", "Практика по Golang");
+
+
+
+    }
+}
