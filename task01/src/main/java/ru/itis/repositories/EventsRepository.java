58341418709cd0d69bd699acf99138bdf920f7a9
ELIZA206 <sergeeff.andrew2018@yandex.ru>
2023-07-02 14:19:40 +0300

    Update EventsRepository.java

--- task01/src/main/java/ru/itis/repositories/EventsRepository.java
+++ task01/src/main/java/ru/itis/repositories/EventsRepository.java
@@ -3,8 +3,12 @@ package ru.itis.repositories;
 import ru.itis.models.Event;
 import ru.itis.models.User;
 
+import java.util.List;
+
 public interface EventsRepository extends CrudRepository<Event> {
     void saveUserToEvent(User user, Event event);
 
     Event findByName(String nameEvent);
+
+    List<Event> findAllByMembersContains(User user);
 }
