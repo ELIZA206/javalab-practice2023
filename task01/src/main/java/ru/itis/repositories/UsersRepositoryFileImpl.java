831d9b94060ee16999c68f10c7b43a9daa78fcf2
ELIZA206 <sergeeff.andrew2018@yandex.ru>
2023-07-02 14:19:55 +0300

    Update UsersRepositoryFileImpl.java

--- task01/src/main/java/ru/itis/repositories/UsersRepositoryFileImpl.java
+++ task01/src/main/java/ru/itis/repositories/UsersRepositoryFileImpl.java
@@ -2,12 +2,12 @@ package ru.itis.repositories;
 
 import ru.itis.models.User;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-public class UsersRepositoryFileImpl implements UsersRepository{
+public class UsersRepositoryFileImpl implements UsersRepository {
     private final String fileName;
 
     public UsersRepositoryFileImpl(String fileName) {
@@ -17,8 +17,8 @@ public class UsersRepositoryFileImpl implements UsersRepository{
 
     @Override
     public void save(User model) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))){
-            writer.write(model.getId() + "|" + model.getEmail() + "|" +  model.getPassword());
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))) {
+            writer.write(model.getId() + "|" + model.getEmail() + "|" + model.getPassword());
             writer.newLine();
         } catch (IOException e) {
             throw new IllegalArgumentException(e);
@@ -28,12 +28,27 @@ public class UsersRepositoryFileImpl implements UsersRepository{
     @Override
     public User findByEmail(String emailUser) {
         // TODO: необходимо реализовать
-        if (emailUser.equals("marsel@gmail.com")) {
-            return User.builder()
-                    .id("ef6e1fba-50ed-48be-84bd-263f9ceaa971")
-                    .email("marsel@gmail.com")
-                    .password("qwerty008")
-                    .build();
-        } return null;
+        List<String> lines = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+        for (String line : lines) {
+            String[] inf = line.split("\\|");
+            if (emailUser.equals(inf[1])) {
+                return User.builder()
+                        .id(inf[0])
+                        .email(inf[1])
+                        .password(inf[2])
+                        .build();
+            }
+
+        }
+        return null;
     }
 }
