b719d03f77448943ce3caba2c7363ba47136f10b
ELIZA206 <sergeeff.andrew2018@yandex.ru>
2023-07-08 11:55:47 +0300

    CourRep homework

---
+++ task03/src/main/java/org/itis/models/course.java
@@ -0,0 +1,8 @@
+package org.itis.models;
+
+public class course {
+    private Integer id;
+    private String title;
+    private String startDate;
+    private Integer FinishDate;
+}
---
+++ task03/src/main/java/org/itis/repositories/CoursesRepository.java
@@ -0,0 +1,6 @@
+package org.itis.repositories;
+
+import org.itis.models.course;
+
+public interface CoursesRepository extends CrudRepository<course>{
+}
